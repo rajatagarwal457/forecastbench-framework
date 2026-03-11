e18dedc6eedadb173013378e2900ee32f7c59be8
rajat <rajat18agarwal@gmail.com>
2026-03-11 12:14:44 +0530

    fix: exa drop-off

--- run.py
+++ run.py
@@ -63,6 +63,7 @@ def setup_logging(date: str, verbose: bool):
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.DEBUG if verbose else logging.INFO)
     ch.setFormatter(logging.Formatter("%(message)s"))
+    our_logger.addHandler(ch)
     root.addHandler(ch)
 
     log.info(f"Logging to {log_file}")
